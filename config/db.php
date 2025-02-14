c0d0d0ebfedc6d717fae7658db361782b3f0a431
Josue Pinto <pintojosue1617@gmail.com>
2025-02-14 16:06:30 -0500

    Descripcionn

--- config/db.php
+++ config/db.php
@@ -1,16 +1,18 @@
 <?php
-// URL de la base de datos proporcionada por Railway
-$databaseUrl = "postgresql://postgres:HMNKqfYUcejmMHlNQhpVhrAYbEnBhCvt@autorack.proxy.rlwy.net:19720/railway";
+$host = "autorack.proxy.rlwy.net";  // Host de la base de datos
+$port = "19720";  // Puerto de la base de datos
+$dbname = "railway";  // Nombre de la base de datos
+$user = "postgres";  // Usuario de la base de datos
+$password = "HMNKqfYUcejmMHlNQhpVhrAYbEnBhCvt";  // Contraseña de la base de datos
 
 try {
-    // Establece la conexión usando la URL
-    $pdo = new PDO("pgsql:" . $databaseUrl);
+    // Conectar a la base de datos
+    $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
+    // Establecer el modo de error
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
     echo "Conexión exitosa a la base de datos";
-
 } catch (PDOException $e) {
-    // Si hay un error en la conexión
     die("Error de conexión: " . $e->getMessage());
 }
 ?>
+
